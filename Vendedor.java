3220492d06bf400c3c20f67761e2ef4c746368ac
Diana60 <dianafo60r@gmail.com>
2016-04-16 15:01:14 +0100

    Update Vendedor.java

--- Vendedor.java
+++ Vendedor.java
@@ -2,47 +2,101 @@ import java.util.ArrayList;
 
 public class Vendedor extends Utilizador {
 
-    ArrayList<String> imoveisEmVenda;
-    ArrayList<String> imoveisVendidos;
+    ArrayList<Imovel> imoveisEmVenda;
+    ArrayList<Imovel> imoveisVendidos;
 
     public Vendedor() {
-        imoveisEmVenda = new ArrayList<>();
-        imoveisVendidos = new ArrayList<>();
+        imoveisEmVenda = new ArrayList<Imovel>();
+        imoveisVendidos = new ArrayList<Imovel>();
     }
 
-    public Vendedor(String email, String nom, String pass, String mor, String dataN) {
+    public Vendedor(String email, String nom, String pass, String mor, String dataN, ArrayList<Imovel> iVenda, ArrayList<Imovel> iVendidos) {
         super(email, nom, pass, mor, dataN);
-        imoveisEmVenda = new ArrayList<>();
-        imoveisVendidos = new ArrayList<>();
+        iVenda = new ArrayList<>();
+        iVendidos = new ArrayList<>();
     }
 
     public Vendedor(Utilizador vendedor) {
-        this(vendedor.getEmail(), vendedor.getNome(),
-                vendedor.getPassword(), vendedor.getMorada(), vendedor.getDataNascimento());
+        //this(vendedor.getEmail(), vendedor.getNome(),
+          //      vendedor.getPassword(), vendedor.getMorada(), vendedor.getDataNascimento());
     }
 
-
-    public ArrayList<String> getImoveisEmVenda() {
-        return imoveisEmVenda;
+    public Vendedor(Vendedor vend){
+        super(vend);
+        setImoveisEmVenda(vend.getImoveisEmVenda());
+        setImoveisVendidos(vend.getImoveisVendidos());
+    }
+    
+    public ArrayList<Imovel> getImoveisEmVenda() {
+        ArrayList<Imovel> res = new ArrayList<Imovel>();
+        for(Imovel i : imoveisEmVenda){
+            res.add(i.clone());
+        }
+        return res;
+    }
+    
+    public ArrayList<Imovel> getImoveisVendidos(){
+        ArrayList<Imovel> res = new ArrayList<Imovel>();
+        for(Imovel i : imoveisVendidos){
+            res.add(i.clone());
+        }
+        return res;
+    }
+    
+    public void setImoveisEmVenda(ArrayList<Imovel> imoveisEmVenda){
+        this.imoveisEmVenda.clear();
+        for(Imovel i : imoveisEmVenda){
+            this.imoveisEmVenda.add(i.clone());
+        }
     }
 
-    public void setImoveisEmVenda(ArrayList<String> imoveisEmVenda) {
-        this.imoveisEmVenda = imoveisEmVenda;
+    public void setImoveisVendidos(ArrayList<Imovel> imoveisVendidos) {
+        this.imoveisVendidos.clear();
+        for(Imovel i : imoveisVendidos){
+            this.imoveisVendidos.add(i.clone());
+        }
+    }
+    
+    public Vendedor clone(){
+        return new Vendedor(this);
+    }
+    
+    public boolean equals(Object o){
+        if(this == o){
+            return true;
+        }
+        if(o == null | o.getClass() != this.getClass()){
+            return false;
+        }
+        Vendedor v = (Vendedor) o;
+        if(imoveisEmVenda.size() != v.getImoveisEmVenda().size()){
+            return false;
+        }
+        if(imoveisVendidos.size() != v.getImoveisVendidos().size()){
+            return false;
+        }
+        for(int i=0; i<imoveisEmVenda.size(); i++){
+            if(!imoveisEmVenda.get(i).equals(v.getImoveisEmVenda().get(i))){
+                return false;
+            }
+        }
+        for(int i=0; i<imoveisVendidos.size(); i++){
+            if(!imoveisVendidos.get(i).equals(v.getImoveisVendidos().get(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+        for(Imovel imovel : imoveisEmVenda){
+            s.append(imoveisEmVenda.toString() +"\n");
+        }
+        for(Imovel imovel : imoveisVendidos){
+            s.append(imoveisVendidos.toString() + "\n");
+        }
+        return s.toString();
     }
 
-    public void setImovelEmVenda(String idImovel) {
-
-    }
-
-    public ArrayList<String> getImoveisVendidos() {
-        return imoveisVendidos;
-    }
-
-    public void setImoveisVendidos(ArrayList<String> imoveisVendidos) {
-        this.imoveisVendidos = imoveisVendidos;
-    }
-
-    public void setImovelComoVendido(String idImovel) {
-
-    }
 }
